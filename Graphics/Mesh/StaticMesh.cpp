84bf479a293713c2048fcc1caa7d9c176679ddc3
Jose Contreras Pereira <jcontreraspereira@gmail.com>
2015-11-30 17:00:06 +0100

    Add MaxScript Exporter.

--- Graphics/Mesh/StaticMesh.cpp
+++ Graphics/Mesh/StaticMesh.cpp
@@ -1,7 +1,11 @@
 #include "StaticMesh.h"
+#include <fstream>
 
 
-CStaticMesh::CStaticMesh(const CXMLTreeNode &TreeNode) : CNamed(TreeNode)
+CStaticMesh::CStaticMesh(const CXMLTreeNode &TreeNode) 
+	: CNamed(TreeNode)
+	, m_NumFaces(0)
+	, m_NumVertexs(0)
 {
 	Load(TreeNode.GetPszProperty("filename"));
 }
@@ -14,6 +18,8 @@ CStaticMesh::~CStaticMesh()
 //Llegir fitxer .mesh
 bool CStaticMesh::Load(const std::string &FileName)
 {
+	std::ifstream file(FileName.c_str(), std::ios::in | std::ios::binary);
+
 	return true;
 }
 
